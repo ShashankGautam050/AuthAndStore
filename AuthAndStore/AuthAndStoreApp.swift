ce2212b297d040085acc0d54963922f93b4b4c98
Shashank Gautam <shashank320722@gmail.com>
2026-02-03 12:41:02 +0530

    Initial Commit

---
+++ AuthAndStore/AuthAndStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthAndStoreApp.swift
+//  AuthAndStore
+//
+//  Created by Shashank Gautam on 03/02/26.
+//
+
+import SwiftUI
+
+@main
+struct AuthAndStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
